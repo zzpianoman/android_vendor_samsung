6135db7fad66e58846b3de6dd7f473dc20f8b28f
root <justinjams@gmail.com>
2015-03-22 23:16:15 -0400

    Add maguro variant, commonize vendor files for tuna, toro and maguro

---
+++ maguro/maguro-vendor-blobs.mk
@@ -0,0 +1,4 @@
+PRODUCT_COPY_FILES += \
+	vendor/samsung/maguro/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
+	vendor/samsung/maguro/proprietary/vendor/firmware/bcm4330.hcd:system/vendor/firmware/bcm4330.hcd \
+	vendor/samsung/maguro/proprietary/vendor/lib/libsec-ril.so:system/vendor/lib/libsec-ril.so
